a0bf122090c17ac812a26eb1b9fb3fd562e9e850
Mask13 <aaron.r.samuel@gmail.com>
2022-03-06 16:41:35 -0500

    working on forms

---
+++ intake.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Register</title>
+  </head>
+  <body>
+    <form name="Intake" id="Intake" method="POST">
+      <label for="Case Number">Case Number: </label>
+      <input id="caseNumber" name="caseNumber" placeholder="Enter Case Number"/>
+      <label for="pass">Password: </label>
+      <input id="password" type="password" name="password" placeholder="Enter Password"/>
+      <input type="submit" name="register"/>
+    </form>
+  </body>
+</html>
---
+++ register.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Register</title>
+  </head>
+  <body>
+    <form name="RegisterForm" id="RegisterForm" method="POST">
+      <label for="email">Username: </label>
+      <input id="username" name="username" placeholder="Enter Username"/>
+      <label for="pass">Password: </label>
+      <input id="password" type="password" name="password" placeholder="Enter Password"/>
+      <input type="submit" name="register"/>
+    </form>
+  </body>
+</html>
