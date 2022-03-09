d47f6eca22691b04f42cb865c1b39462bc4c99a6
Mask13 <aaron.r.samuel@gmail.com>
2022-03-09 16:31:04 -0500

    Update register.php

--- front-end/register.php
+++ front-end/register.php
@@ -13,3 +13,44 @@
     </form>
   </body>
 </html>
+<?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if(        isset($_POST['email'])
+        && isset($_POST['password'])
+        && isset($_POST['confirm'])
+        ){
+        $pass = $_POST['password'];
+        $conf = $_POST['confirm'];
+        if($pass == $conf){
+                //echo "All good, 'registering user'";
+                $msg = "All good, your registered";
+        }
+        else{
+
+                exit();
+        }
+        //let's hash it
+        $pass = password_hash($pass, PASSWORD_BCRYPT);
+        echo "<br>$pass<br>";
+        //it's hashed
+        require("config.php");
+        $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+        try {
+                $db = new PDO($connection_string, $dbuser, $dbpass);
+                $stmt = $db->prepare("INSERT INTO `Coustomers`
+                        (email, password) VALUES
+                        (:email, :password)");
+                $email = $_POST['email'];
+        $params = array(":email"=> $email, ":password"=> $pass);
+        $stmt->execute($params);
+        echo "<pre>" . var_export($stmt->errorInfo(), true) . "</pre>";
+        }
+        catch(Exception $e){
+                echo $e->getMessage();
+                exit();
+        }
+}
+?>
