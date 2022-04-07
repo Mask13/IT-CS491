a785cacf4d487dc953f357960874a3e1b325c07e
Mask13 <aaron.r.samuel@gmail.com>
2022-04-06 20:59:21 -0400

    Update logout.php

--- front-end/logout.php
+++ front-end/logout.php
@@ -1,3 +1,24 @@
+<?php
+// Initialize the session.
+// If you are using session_name("something"), don't forget it now!
+session_start();
+
+// Unset all of the session variables.
+$_SESSION = array();
+
+// If it's desired to kill the session, also delete the session cookie.
+// Note: This will destroy the session, and not just the session data!
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Finally, destroy the session.
+session_destroy();
+?>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
@@ -15,19 +36,3 @@
     </div>
 	</body>
 </html>
-<?php
-session_start();
-session_unset();
-session_destroy();
-
-//get session cookie and delete/clear it for this session
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-	//clones then destroys since it makes it's lifetime
-	//negative (in the past)
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
-}
-?>
