29f6b4afb69edd3d9d64f9544a487b71fbcd3021
Mask13 <aaron.r.samuel@gmail.com>
2022-04-08 18:32:13 -0400

    Update Satisfaction-Survey.php

--- front-end/Satisfaction-Survey.php
+++ front-end/Satisfaction-Survey.php
@@ -23,13 +23,16 @@ $data = $stmt->fetchAll();
   <head>
     <meta charset="utf-8">
     <title>Satsifaction Survey</title>
-    <button style= "float:right;"type="button" onclick="location.href = 'Logout.php';"
-          name="Login"> Logout
-    </button>
-    <button style= "float:right;"type="button" onclick="location.href = 'home.php';"
-          name="Login"> Home
-    </button>
-  </head><br><br>
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/jumbotron/">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </head>
   <style>
     .button {
       padding: 7px 14px;
@@ -58,7 +61,110 @@ $data = $stmt->fetchAll();
       margin-bottom: 5px;
     }
   </style>
+
+  <header>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light" style="font-family: sans-serif; font-size: 120%; background-color: #36096d; background-image: linear-gradient(315deg, #36096d 0%, #37d5d6 74%);">
+
+    <a class="navbar-brand" style ="color: black; font-size: 160%"><b>NJ-FSO</b></a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarsExampleDefault" aria-expanded="true" aria-label="Toggle navigation">
+
+      <span class="navbar-toggler-icon"></span>
+
+    </button>
+
+
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+      <ul class="navbar-nav mr-auto">
+
+        <li class="nav-item">
+
+          <a class="nav-link" href="home.php"style ="color: black;">Home</a>
+
+        </li>
+
+        <li class="nav-item">
+
+          <a class="nav-link" href="warmline.php"style ="color: black;">Warmline Contact</a>
+
+        </li>
+
+        <li class="nav-item dropdown">
+
+          <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown">
+      Community Outreach
+      </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+
+            <a class="dropdown-item" href="out-Meet.php">FSO-Meeting and Training Attendance</a>
+            <a class="dropdown-item" href="InitMeet.php">Assigned FSO Meetings</a>
+            <a class="dropdown-item" href="Satisfaction-Survey.php">Family Satisfaction Survey</a>
+
+          </div>
+        </li>
+
+
+
+        <li class="nav-item dropdown">
+
+          <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown">
+
+            Family Support
+
+          </a>
+
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+
+            <a class="dropdown-item" href="intake.php">Participant Enrollment</a>
+
+
+
+            <a class="dropdown-item" href="InitMeet.php">Assigned FSO Meetings</a>
+
+
+
+            <a class="dropdown-item" href="Satisfaction-Survey.php">Family Satisfaction Survey</a>
+
+
+
+          </div>
+
+        </li>
+
+        <li class="nav-item dropdown">
+
+          <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown">
+
+            Display
+
+          </a>
+
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+
+            <a class="dropdown-item" href="display_intake.php">Intake Display</a>
+
+            <a class="dropdown-item" href="display_satisfaction.php">Display Satisfaction</a>
+
+            <a class="dropdown-item" href="display_meet.php">Display Meet</a>
+
+            <a class="dropdown-item" href="display_warm.php">Display Warmline</a>
+
+          </div>
+
+        </li>
+
+      </ul>
+
+        <button style="float:right" type="button" class="btn btn-outline-success my-2 my-sm-0" onclick="location.href = 'logout.php';">Logout</button>
+
+    </div>
+    </nav>
+  </header>
+
   <body>
+    <div class="col">
     <form class ="form1" name="Intake" id="Intake" method="POST">
       <font> Satsifaction Survey: </font> <br>
       <label for="prompt">Select the Family.</label>
@@ -150,22 +256,23 @@ $data = $stmt->fetchAll();
       </select><br>
       <input class="button" type="submit" name="submit"/>
     </form>
+    </div>
   </body>
 </html>
 
 <?php
 	if($_POST){
-		
+
 		try{
 			$stmt = $db->prepare("INSERT INTO `survey`
                         VALUES (DEFAULT, :prompt,:court,:inform,:knowledge,:advocacy,:goals,:qol,:recommend,:fid)");
 			$params = array(":prompt"=> $_POST["prompt"],":court"=> $_POST["curteous"], ":inform"=> $_POST["informed"],":knowledge"=> $_POST["answerQuestions"],":advocacy"=> $_POST["advocacySkills"],
 							":goals"=> $_POST["progress"],":qol"=> $_POST["qualityOfLife"],":recommend"=> $_POST["recommend"], ":fid"=> $_POST["family"]);
-			
+
 			$stmt->execute($params);
 			#echo "<pre>" . var_export($stmt->errorInfo(), true) . "</pre>";
 		}
-		
+
 		catch(Exception $e){
                 echo $e->getMessage();
                 exit();
