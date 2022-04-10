8acb5effc9fb1c25d340b28ac21c5e629fba81db
JAeadeh <jaeadeh@gmail.com>
2022-04-10 06:40:26 -0400

    FANS display

--- front-end/display_fans.php
+++ front-end/display_fans.php
@@ -1,3 +1,7 @@
+<?php
+      include_once('navbar.php');
+  ?>
+
 <html lang="en" dir="ltr">
 	<head>
 		<title>display_Fans</title>
@@ -8,9 +12,7 @@
 					name="Login"> Home
 		</button>
 	</head>
-	<?php
-      include_once('navbar.php');
-  ?>
+
 </html>
 
 <?php
@@ -20,32 +22,51 @@
 	if(!($_SESSION['role']>=0)){
 	header("Location: index.php");
 }
-
-
 		error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 		ini_set('display_errors', 1);
-       require "config.php";
-       $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
-       $db= new PDO($connection_string, $dbuser, $dbpass);
-	   echo "<br>";
-	   echo "<table border='1'>";
-       try{
-
+		require "config.php";
+		$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+		$db= new PDO($connection_string, $dbuser, $dbpass);
+		#$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE);
+		echo "<br>";
+		echo "<table border='1'>";
+		try{
 			$q = $db->prepare("DESCRIBE fans");
 			$q->execute();
 			$table_fields = $q->fetchAll(PDO::FETCH_COLUMN);
 			foreach ($table_fields as $col_name) {
 				echo "<td>" .$col_name."</td>";
 				}
-			$stmt = $db->query('SELECT * FROM fans where')->fetchall(PDO::FETCH_ASSOC);
-			foreach ($stmt as $row) {
-				foreach ($row as $value){
-					echo "<td>" . $value . "</td>";
+			$stmt = $db->prepare('SELECT fid FROM family WHERE uid=:u_id');
+
+			$stmt->execute(['u_id' => intval($_SESSION["ID"])]);
+
+			$data = $stmt->fetchAll();
+			foreach ($data as $family_id){
+				$stmt = $db->prepare('SELECT * FROM fans WHERE f_id=:fam_id');
+				$stmt->execute(['fam_id' => $family_id[0]]);
+				$data2 = $stmt->fetchAll(PDO::FETCH_ASSOC);
+				
+				#echo "<pre>" . var_export($stmt->errorInfo(), true) . "</pre>";
+				#var_dump($data2);
+				
+				foreach($data2 as $row){
+					echo "<tr>";
+					#var_dump($row);
+					foreach ($row as $value){
+						
+						echo "<td>" . $value . "</td>";
 				  }
 				  
-				  
-			
+				}
 			}
+				
+		}
+			
+	   catch(Exception $e){
+			echo $e->getMessage();
+			exit();
+	   }
 			
 			
 ?>
