244efd4fb6e9768a7d797014f57132f1e0038d24
Mask13 <aaron.r.samuel@gmail.com>
2022-04-11 23:37:21 -0400

    displays update

--- front-end/display_family.php
+++ front-end/display_family.php
@@ -1,10 +1,24 @@
 <?php
       include_once('navbar.php');
 ?>
+<html>
+	<header>
+	<link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/jumbotron/">
+
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+	</head>
+</html>
 <?php
 	require("config.php");
 	session_start();
-
+	
 	if(!(isset($_SESSION['role']))){
   header("Location: index.php");
 }
@@ -14,7 +28,7 @@
 
 	$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 	$db= new PDO($connection_string, $dbuser, $dbpass);
-	$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE);
+	$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE); 
 
 	echo "<br>";
 	echo "<table border='1'>";
@@ -23,13 +37,13 @@
 	echo "<td> Last Name </td>";
 	echo "<td> Case Number </td>";
 	echo "<td> Assigned Employee </td>";
-
+	
 	try{
-
+		
 		$stmt = $db->prepare('SELECT firstname, lastname, fid, uid FROM family WHERE uid=:id');
 		$stmt->execute(['id' => intval($_SESSION["ID"])]);
 		$data = $stmt->fetchAll();
-
+		
 		$stmt = $db->prepare('SELECT username FROM users WHERE id=:id');
 		$stmt->execute(['id' => intval($_SESSION["ID"])]);
 		$data1 = $stmt->fetchAll();
@@ -38,7 +52,7 @@
 			$stmt = $db->prepare('SELECT casenumber FROM cases WHERE fid=:id');
 			$stmt->execute(['id' => intval($row["fid"])]);
 			$data2 = $stmt->fetchAll();
-
+			
 			echo "<td>" . $row["fid"] . "</td>";
 			echo "<td>" . $row["firstname"] . "</td>";
 			echo "<td>" . $row["lastname"] . "</td>";
@@ -47,7 +61,7 @@
 		}
 
 	}
-
+	
 	catch(Exception $e){
 			echo $e->getMessage();
 			exit();
--- front-end/display_intake.php
+++ front-end/display_intake.php
@@ -2,17 +2,19 @@
       include_once('navbar.php');
   ?>
 
-<html lang="en" dir="ltr">
-	<head>
-		<title>display_intake</title>
-		<button style= "float:right;"type="button" onclick="location.href = 'Logout.php';"
-					name="Login"> Logout
-		</button>
-		<button style= "float:right;"type="button" onclick="location.href = 'home.php';"
-					name="Login"> Home
-		</button>
-	</head>
+<html>
+	<header>
+	<link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/jumbotron/">
 
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+	</head>
 </html>
 <?php
 
