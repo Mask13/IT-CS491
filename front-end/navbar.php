2b27f8a4c8ffe6df7be71027b4ec3e6db9467af8
Mask13 <aaron.r.samuel@gmail.com>
2022-04-13 18:13:17 -0400

    CSS updates wooooooo

--- front-end/navbar.php
+++ front-end/navbar.php
@@ -1,9 +1,17 @@
 <?php session_start(); ?>
-
+<style>
+  hotbox{
+    border-radius:20px;
+  }
+  hotbox:hover{
+    transition: .25s;
+    background-color:black;
+  }
+</style>
 <header>
-<nav class="navbar navbar-expand-lg navbar-light bg-light" style="font-family: sans-serif; font-size: 120%; background-color: #36096d; background-image: linear-gradient(315deg, #36096d 0%, #37d5d6 74%);">
+<nav class="navbar navbar-expand-lg navbar-dark bg-light" style="font-family: sans-serif; font-size: 120%; background-color: #36096d; background-image: linear-gradient(315deg, #36096d 10%, #FFAA00 75%);">
 
-  <a class="navbar-brand" style ="color: black; font-size: 160%"><b>NJ-FSO</b></a>
+  <a class="navbar-brand" href="home.php" style ="color: black; font-size: 160%"><b>NJ-FSO</b></a>
 
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarsExampleDefault" aria-expanded="true" aria-label="Toggle navigation">
 
@@ -16,82 +24,83 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
     <ul class="navbar-nav mr-auto">
-
+      <hotbox>
       <li class="nav-item">
-
-        <a class="nav-link" href="home.php"style ="color: black;">Home</a>
-
+        <a class="nav-link" href="warmline.php" style="color:white;">Warmline Contact</a>
       </li>
+      </hotbox>
+      <hotbox>
+        <li class="nav-item dropdown">
 
-      <li class="nav-item">
+          <a class="nav-link dropdown-toggle" style="color:white;" id="navbarDropdown" role="button" data-toggle="dropdown">
+            Community Outreach
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="out-Meet.php">FSO-Meeting and Training Attendance</a>
+            <a class="dropdown-item" href="InitMeet.php">Assigned FSO Meetings</a>
+            <a class="dropdown-item" href="CommMeet.php"> Community Initiatives and Meetings given by the FSO</a>
+          </div>
+        </li>
+      </hotbox>
+      <hotbox>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" style="color:white;" id="navbarDropdown" role="button" data-toggle="dropdown">
+            Family Support
+          </a>
 
-        <a class="nav-link" href="warmline.php"style ="color: black;">Warmline Contact</a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="intake.php">Participant Enrollment</a>
+            <a class="dropdown-item" href="InitMeet.php">Assigned FSO Meetings</a>
+            <a class="dropdown-item" href="FANAssesment.php">FANS Assesment</a>
+            <a class="dropdown-item" href="Satisfaction-Survey.php">Family Satisfaction Survey</a>
+            <a class="dropdown-item" href="prognotes.php">Family Progress Notes</a>
+          </div>
 
-      </li>
+        </li>
+      </hotbox>
+      <hotbox>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" style="color:white;" id="navbarDropdown" role="button" data-toggle="dropdown">
+              Youth Partnership
+          </a>
 
-      <li class="nav-item dropdown">
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="Youth-Partnership-Inake.php">Youth Partnership Intake</a>
+            <a class="dropdown-item" href="Youth-Partnership-Meeting.php">Youth Partnership Meeting</a>
+            <a class="dropdown-item" href="Youth-Partnership-Task.php">Youth Partnership Task</a>
+          </div>
 
-        <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown">
-    Community Outreach
-    </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        </li>
+      </hotbox>
+      <hotbox>
+        <li class="nav-item dropdown">
 
-          <a class="dropdown-item" href="out-Meet.php">FSO-Meeting and Training Attendance</a>
-          <a class="dropdown-item" href="InitMeet.php">Assigned FSO Meetings</a>
-          <a class="dropdown-item" href="Satisfaction-Survey.php">Family Satisfaction Survey</a>
+          <a class="nav-link dropdown-toggle" style="color:white;" id="navbarDropdown" role="button" data-toggle="dropdown">
 
-        </div>
-      </li>
+            Display
 
+          </a>
 
-      <li class="nav-item dropdown">
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
 
-        <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown">
+            <a class="dropdown-item" href="display_intake.php">Intake Display</a>
 
-          Family Support
+            <a class="dropdown-item" href="display_satisfaction.php">Display Satisfaction</a>
 
-        </a>
+            <a class="dropdown-item" href="display_meet.php">Display FSO Meeting</a>
 
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="display_warm.php">Display Warmline</a>
 
-          <a class="dropdown-item" href="intake.php">Participant Enrollment</a>
+            <a class="dropdown-item" href="display_comm.php">Display Community Meetings</a>
 
+            <a class="dropdown-item" href="display_fans.php">Display FAN Assesment</a>
 
+            <a class="dropdown-item" href="display_outmeet.php">Display Outreach Meeting</a>
 
-          <a class="dropdown-item" href="InitMeet.php">Assigned FSO Meetings</a>
+          </div>
 
-          <a class="dropdown-item" href="FANAssesment.php">FANS Assesment</a>
-
-          <a class="dropdown-item" href="Satisfaction-Survey.php">Family Satisfaction Survey</a>
-
-
-
-        </div>
-
-      </li>
-
-      <li class="nav-item dropdown">
-
-        <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown">
-
-          Display
-
-        </a>
-
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-
-          <a class="dropdown-item" href="display_intake.php">Intake Display</a>
-
-          <a class="dropdown-item" href="display_satisfaction.php">Display Satisfaction</a>
-
-          <a class="dropdown-item" href="display_meet.php">Display Meet</a>
-
-          <a class="dropdown-item" href="display_warm.php">Display Warmline</a>
-
-        </div>
-
-      </li>
-
+        </li>
+      </hotbox>
     </ul>
 
       <button style="float:right" type="button" class="btn btn-outline-success my-2 my-sm-0" onclick="location.href = 'logout.php';">Logout</button>
